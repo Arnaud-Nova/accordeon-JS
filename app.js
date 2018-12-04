6a20949c48a2e300ffbdbcbdc14ac623e93103f5
Arnaud-Nova <calmelsarnaud@gmail.com>
2018-12-04 14:32:52 +0100

    syntaxe forEach sauce MDN :)

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = {
   init : function() {
     app.question = document.querySelectorAll('.titre');
     // Boucle forEach pour lancer le listener sur chaque élément sélectionné par querySelectorALl
-    app.question.forEach(element => {
+    app.question.forEach(function(element) {
       element.addEventListener('click', app.openAnswer);
     });
     
@@ -12,7 +12,7 @@ var app = {
 
   openAnswer: function(evt) {
     // je referme toute les questions
-    app.question.forEach(element => {
+    app.question.forEach(function(element) {
       element.className = 'titre';
     });
     // j'ouvre la question cliquée
